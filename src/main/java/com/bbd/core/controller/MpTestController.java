78f5d07e5ca0c891ee3b4f843b8b64c3fd378e35
臧涛 <zangtao-sj@bestpay.com.cn>
2020-01-07 17:56:44 +0800

    添加shiro

---
+++ src/main/java/com/bbd/core/controller/MpTestController.java
@@ -0,0 +1,16 @@
+package com.bbd.core.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zangtao
+ * @date 2020/1/7 13:42
+ */
+@RestController
+@RequestMapping("mpTest")
+public class MpTestController {
+
+
+}
