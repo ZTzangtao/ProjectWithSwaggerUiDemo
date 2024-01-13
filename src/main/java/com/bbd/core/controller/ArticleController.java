a1a46294ea5f0be3137f814f21bd040ab541a6a9
zangtao <TZANG1@ford.com>
2024-01-13 19:11:00 +0800

    阿里云api跟车架号查询信息

--- src/main/java/com/bbd/core/controller/ArticleController.java
+++ src/main/java/com/bbd/core/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.bbd.core.controller;
 
 import com.bbd.common.annotation.ArticleOperation;
 import com.bbd.common.base.Response;
+import com.bbd.common.util.HttpUtils;
 import com.bbd.core.dto.ArticleEditVo;
 import com.bbd.core.dto.ArticleVo;
 import com.bbd.core.service.ILaArticleService;
@@ -10,10 +11,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <p>
@@ -80,5 +85,42 @@ public class ArticleController {
     ) {
         return iLaArticleService.onlineArticle(showId, type);
     }
+
+
+
+        public static void main(String[] args) {
+            String host = "https://jmfaceip.market.alicloudapi.com";
+            String path = "/vehicle/vin-query";
+            String method = "POST";
+            String appcode = "d8d19719f3fc4454b30982f05da9236a";
+            Map<String, String> headers = new HashMap<String, String>();
+            //最后在header中的格式(中间是英文空格)为Authorization:APPCODE 83359fd73fe94948385f570e3c139105
+            headers.put("Authorization", "APPCODE " + appcode);
+            //根据API的要求，定义相对应的Content-Type
+            headers.put("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+            Map<String, String> querys = new HashMap<String, String>();
+            Map<String, String> bodys = new HashMap<String, String>();
+            bodys.put("vin", "LBV31FH0XRM724749");
+
+
+            try {
+                /**
+                 * 重要提示如下:
+                 * HttpUtils请从
+                 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/src/main/java/com/aliyun/api/gateway/demo/util/HttpUtils.java
+                 * 下载
+                 *
+                 * 相应的依赖请参照
+                 * https://github.com/aliyun/api-gateway-demo-sign-java/blob/master/pom.xml
+                 */
+                HttpResponse response = HttpUtils.doPost(host, path, method, headers, querys, bodys);
+                System.out.println(response.toString());
+                //获取response的body
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
 
 }
